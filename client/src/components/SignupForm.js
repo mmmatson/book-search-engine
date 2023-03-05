461bc7d4e3e9e90f426f9de233b4d706b3e8040a
mmm <margaret.m.matson@gmail.com>
2023-03-05 11:29:05 -0600

    fixed bugs

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { ADD_USER } from "../utils/mutations";
 import { useMutation } from '@apollo/client';
-
+import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
-import Auth from '../utils/auth';
-import { SAVE_BOOK } from "../utils/mutations";
 import { useMutation } from '@apollo/client';
-
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { SAVE_BOOK } from "../utils/mutations";
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import Auth from '../utils/auth';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
